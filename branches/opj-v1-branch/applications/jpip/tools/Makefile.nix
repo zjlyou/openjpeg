9fae8d9c6c7ff0eefe218f885e0f4f382cea8b4d
antonin <antonin@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-07-10 18:36:17 +0000

    opj-v1-branch created

---
+++ branches/opj-v1-branch/applications/jpip/tools/Makefile.nix
@@ -0,0 +1,24 @@
+LIBDIR = ../libopenjpip
+LIBFNAME = $(LIBDIR)/libopenjpip_local.a
+CFLAGS  = -O3 -Wall -I$(LIBDIR)
+LDFLAGS = -L$(LIBDIR) -lm -lopenjpip_local
+
+ALL = jpip_to_jp2 jpip_to_j2k test_index addXMLinJP2
+
+all: t_indexer $(ALL)
+  
+t_indexer:
+	make -C indexer -f Makefile.nix
+
+jpip_to_jp2: jpip_to_jp2.o $(LIBFNAME)
+	    $(CC) $(CFLAGS) $< $(LDFLAGS) -o $@
+
+jpip_to_j2k: jpip_to_j2k.o $(LIBFNAME)
+	    $(CC) $(CFLAGS) $< $(LDFLAGS) -o $@
+
+test_index: test_index.o $(LIBFNAME)
+	    $(CC) $(CFLAGS) $< $(LDFLAGS) -o $@
+
+clean:
+	rm -f $(ALL) *.o *~
+	make clean -C indexer -f Makefile.nix
