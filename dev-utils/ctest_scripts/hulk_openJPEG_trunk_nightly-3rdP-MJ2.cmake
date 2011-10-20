95d63dacf20b7663b8784b7d880f39e39140771c
savmickael <savmickael@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-10-20 07:27:31 +0000

    [dev-utils] clean some ctest script, deactivate some ctest_test for build with no specitfic test suite

--- dev-utils/ctest_scripts/hulk_openJPEG_trunk_nightly-3rdP-MJ2.cmake
+++ dev-utils/ctest_scripts/hulk_openJPEG_trunk_nightly-3rdP-MJ2.cmake
@@ -17,18 +17,17 @@ SET (CTEST_SOURCE_DATA_DIRECTORY  "$ENV{HOME}/Dashboard/src/OpenJPEG/opj-data")
 # User inputs:
 SET( CTEST_CMAKE_GENERATOR      "Unix Makefiles" )    # What is your compilation apps ? (Eclipse CDT4 - Unix Makefiles)
 SET( CTEST_CMAKE_COMMAND        "cmake" )
-SET( CTEST_BUILD_COMMAND        "/usr/bin/make " )
+SET( CTEST_BUILD_COMMAND        "/usr/bin/make -j2" )
 SET( CTEST_SITE                 "hulk.c-s.fr" )       # Generally the output of hostname
 SET( CTEST_BUILD_CONFIGURATION  Debug)                # What type of build do you want ?
 SET( CTEST_BUILD_NAME           "Ubuntu10.04-64bits-trunk-${CTEST_BUILD_CONFIGURATION}-3rdP-MJ2") # Build Name
 
-set( CACHE_CONTENTS "
+SET( CACHE_CONTENTS "
 CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 
 CMAKE_C_FLAGS:STRING= -Wall 
 
 BUILD_TESTING:BOOL=TRUE
-BUILD_EXAMPLES:BOOL=TRUE
 
 OPJ_DATA_ROOT:PATH=${CTEST_SOURCE_DATA_DIRECTORY}
 
@@ -47,13 +46,9 @@ ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 
 # Update method 
 # repository: http://openjpeg.googlecode.com/svn/trunk (openjpeg-read-only)
-# need to use https for CS machine
 SET( CTEST_SVN_COMMAND      "/usr/bin/svn")
 
-
 # 3. cmake specific:
-#set( CTEST_NOTES_FILES      "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
-
 ctest_empty_binary_directory( "${CTEST_BINARY_DIRECTORY}" )
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "${CACHE_CONTENTS}")
 
@@ -63,6 +58,7 @@ ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}")
+# No test suite specific for MJ2
+# ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_submit()
 
