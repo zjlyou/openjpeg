547398d02ee1bd7b03f1ab5a654ba2a14e57dd4b
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-01 16:53:57 +0000

    Fix script for debian. when kdu_expand is in /usr/bin

--- dev-utils/scripts/generate_big_files.sh
+++ dev-utils/scripts/generate_big_files.sh
@@ -8,14 +8,15 @@
 # $4 : input image size 
 # $5 : number of tiles in x and y file
 # Example: 
-# ./generate_big_files lena2048color.tiff lena_massive_rotate_2.j2k 81920 2048 40 => output file is bigger than 2^32
-# ./generate_big_files lena512color.tiff lena_massive_rotate.j2k 35840 512 70 =>  => output file is bigger than 2^31
+# ./generate_big_files.sh lena2048color.tiff lena_massive_rotate_2.j2k 81920 2048 40 => output file is bigger than 2^32
+# ./generate_big_files.sh lena512color.tiff lena_massive_rotate.j2k 35840 512 70 =>  => output file is bigger than 2^31
 
-# You should export the path to your kakadu in the library path
+# You should export the path to your kakadu in the library path:
 # export LD_LIBRARY_PATH="/home/mickael/dev/src/OpenJPEG/kakadu_bin/"
+# export PATH=$PATH:"/home/mickael/dev/src/OpenJPEG/kakadu_bin/"
 
-i_parity = 0 # 0 => i is even and 1 => i is odd
-j_parity = 0 # 0 => j is even and 1 => j is odd
+i_parity=0 # 0 => i is even and 1 => i is odd
+j_parity=0 # 0 => j is even and 1 => j is odd
 
 for ((i=0 ; $5-$i ; i++)) do
   let "i_parity = i % 2"
@@ -25,12 +26,12 @@ for ((i=0 ; $5-$i ; i++)) do
 
     if [ "$j_parity" = "1" ] && [ "$i_parity" = "0" ]   
     then
-      ./kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet -rotate 180 Creversible=yes Clayers=9 Clevels=6 
+      kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet -rotate 180 Creversible=yes Clayers=9 Clevels=6 
     elif [ "$j_parity" = "0" ] && [ "$i_parity" = "1" ]  
     then
-      ./kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet -rotate 180 Creversible=yes Clayers=9 Clevels=6 
+      kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet -rotate 180 Creversible=yes Clayers=9 Clevels=6 
     else
-      ./kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet Creversible=yes Clayers=9 Clevels=6 
+      kdu_compress -i $1 -o $2 Sdims="{$3,$3}" Stiles="{$4,$4}" -frag $i,$j,1,1 -quiet Creversible=yes Clayers=9 Clevels=6 
     fi
 
   done
