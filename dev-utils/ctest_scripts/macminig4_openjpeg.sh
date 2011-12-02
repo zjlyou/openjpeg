ecc2fb2cd9b2bf85d3007cd0abe83c71dd563c8f
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-02 11:26:42 +0000

    make it a release build

--- dev-utils/ctest_scripts/macminig4_openjpeg.sh
+++ dev-utils/ctest_scripts/macminig4_openjpeg.sh
@@ -2,6 +2,6 @@
 
 cd $HOME/Dashboards/ctest_scripts && svn up
 
-ctest -S macminig4-trunk-nightly-debug-linux_gcc41.ctest -V -O gcc41.log 
-ctest -S macminig4-trunk-nightly-debug-linux_gcc43.ctest -V -O gcc43.log 
-ctest -S macminig4-trunk-nightly-debug-linux_gcc44.ctest -V -O gcc44.log
+for i in `ls macminig4-*.ctest`; do
+  ctest -S $i -V -O $i.log 
+done
