7eb716758e6c62c48d73d26414cb423988629ce1
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-05 09:38:36 +0000

    Add x86 and x64

--- dev-utils/ctest_scripts/hpdesk_openjpeg.bat
+++ dev-utils/ctest_scripts/hpdesk_openjpeg.bat
@@ -1,13 +1,24 @@
+cd $HOME/Dashboards/ctest_scripts && svn up
+
+setlocal
 @rem Use Visual Studio 2010:
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-
-cd $HOME/Dashboards/ctest_scripts && svn up
-
-@rem loop over all hpdesk-*.ctest files:
+@rem loop over all hpdesk-*x86.ctest files:
 set ctestlist=
-for %%a in ("hpdesk-*.ctest") do call :concat "%%a"
+for %%a in ("hpdesk-*x86.ctest") do call :concat "%%a"
 ctest -S %ctestlist% -V -O %ctestlist%.log
+endlocal
+
+setlocal
+@rem Use Visual Studio 2010 for x64
+call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+
+@rem loop over all hpdesk-*x64.ctest files:
+set ctestlist=
+for %%a in ("hpdesk-*x64.ctest") do call :concat "%%a"
+ctest -S %ctestlist% -V -O %ctestlist%.log
+endlocal
 
 goto :EOF
 :concat
