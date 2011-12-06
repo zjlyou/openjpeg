54fd663339b9c6e1177b1f0791823b71c1cc1afa
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-06 09:44:34 +0000

    Create a read-only mirror on github

--- dev-utils/ctest_scripts/macminig4_openjpeg.sh
+++ dev-utils/ctest_scripts/macminig4_openjpeg.sh
@@ -5,3 +5,8 @@ cd $HOME/Dashboards/ctest_scripts && svn up
 for i in `ls macminig4-*.ctest`; do
   ctest -S $i -V -O $i.log 
 done
+
+# Update git clone on github:
+cd $HOME/Dashboards/git/trunk
+git svn rebase
+git push --mirror mathieu
