19ea10ec7055d3d61535a3927b50200a808e4e5c
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-08 09:58:20 +0000

    Update from today

--- dev-utils/scripts/script.sh
+++ dev-utils/scripts/script.sh
@@ -2,5 +2,5 @@
 # Convert a CDash HTML page into wiki formatting
 # See: http://code.google.com/p/openjpeg/wiki/Release15
 
-wget -O input.html "http://my.cdash.org/viewTest.php?onlyfailed&buildid=265876"
+wget -O input.html "http://my.cdash.org/viewTest.php?onlyfailed&buildid=267948"
 xsltproc --html process.xsl input.html > output.wiki
