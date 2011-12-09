ebca1ba8ff11c0dc0d1b716c0104f7770e0d1170
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-09 09:39:53 +0000

    Add continuous

--- dev-utils/ctest_scripts/ks200451_openjpeg.sh
+++ dev-utils/ctest_scripts/ks200451_openjpeg.sh
@@ -2,6 +2,6 @@
 
 cd $HOME/Dashboards/ctest_scripts && svn up
 
-for i in `ls ks200451-*.ctest`; do
+for i in `ls ks200451-*nightly*.ctest`; do
   ctest -S $i -V -O $i.log 
 done
---
+++ dev-utils/ctest_scripts/ks200451_openjpeg_cont.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ctest -S ks200451-b15-continuous-debug-linux_gcc44.ctest -V -O cont_b15.log
