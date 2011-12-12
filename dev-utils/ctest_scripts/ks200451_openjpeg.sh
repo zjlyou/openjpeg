d64e0082c6a703003f1c63cea24608223c0b1b82
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-12 09:43:37 +0000

    Add start/stop test

--- dev-utils/ctest_scripts/ks200451_openjpeg.sh
+++ dev-utils/ctest_scripts/ks200451_openjpeg.sh
@@ -5,3 +5,11 @@ cd $HOME/Dashboards/ctest_scripts && svn up
 for i in `ls ks200451-*nightly*.ctest`; do
   ctest -S $i -V -O $i.log 
 done
+
+# Prepare JPIP server:
+# http://code.google.com/p/openjpeg/wiki/JPIP
+cd $HOME/Dashboards/MyTests/openjpeg-b15-nightly-linux_gcc44-v1.5-debug
+DESTDIR=$HOME/Dashboards/MyTests/jpip-install make install
+cd $HOME/Dashboards/MyTests/jpip-install/usr/local/bin/
+spawn-fcgi -f ./opj_server -p 3000 -n
+GET http://localhost/myFCGI?quitJPIP
