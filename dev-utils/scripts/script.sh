8fbf6f857e6864de5958e2b43675b70ef17d5a9b
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-12 10:02:14 +0000

    command line args

--- dev-utils/scripts/script.sh
+++ dev-utils/scripts/script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# Usage:
+# ./script.sh "http://my.cdash.org/viewTest.php?onlyfailed&buildid=269778" table2.wiki  
+
 # Convert a CDash HTML page into wiki formatting
 # See: http://code.google.com/p/openjpeg/wiki/Release15
-
-wget -O input.html "http://my.cdash.org/viewTest.php?onlyfailed&buildid=267948"
-xsltproc --html process.xsl input.html > output.wiki
+wget -O input.html "$1"
+xsltproc --html process.xsl input.html > $2
