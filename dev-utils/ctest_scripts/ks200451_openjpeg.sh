8a226f4adfbb8f6b90df0db15d2a8d9bb9eb1653
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2011-12-27 10:09:29 +0000

    prepare jpip tests

--- dev-utils/ctest_scripts/ks200451_openjpeg.sh
+++ dev-utils/ctest_scripts/ks200451_openjpeg.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+set -x
+
+# Prepare JPIP server from yesterday builds:
+# http://code.google.com/p/openjpeg/wiki/JPIP
+cd $HOME/Dashboards/MyTests/openjpeg-b15-nightly-linux_gcc44-v1.5-debug
+DESTDIR=/tmp/jpip-install make install
+cd /tmp/jpip-install/usr/local/bin/
+spawn-fcgi -f ./opj_server -p 3000 -n
 
 cd $HOME/Dashboards/ctest_scripts && svn up
 
+# nightly
 for i in `ls ks200451-*nightly*.ctest`; do
   ctest -S $i -V -O $i.log 
 done
 
-# Prepare JPIP server:
-# http://code.google.com/p/openjpeg/wiki/JPIP
-cd $HOME/Dashboards/MyTests/openjpeg-b15-nightly-linux_gcc44-v1.5-debug
-DESTDIR=$HOME/Dashboards/MyTests/jpip-install make install
-cd $HOME/Dashboards/MyTests/jpip-install/usr/local/bin/
-spawn-fcgi -f ./opj_server -p 3000 -n
+# exp
+for i in `ls ks200451-*experimental*.ctest`; do
+  ctest -S $i -V -O $i.log 
+done
+
+# close server
 GET http://localhost/myFCGI?quitJPIP
