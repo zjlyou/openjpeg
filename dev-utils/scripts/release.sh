018fb38814c9e99c39d9949166ced2ac98b18f84
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2012-01-11 14:53:25 +0000

    refactor code

--- dev-utils/scripts/release.sh
+++ dev-utils/scripts/release.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
-#generate OpenJPEG release on *NIX
+#generate OpenJPEG release on *NIX (both Linux and MacOSX)
 set -x
 
 # get tmpdir:
 TMPDIR=/tmp/openjpeg_release
 
+isOSX="`uname -s | grep -i Darwin`"
+if [ "$isOSX" != "" ]; then
+osxVerFul=`system_profiler |grep 'System Version'| sed -e"s/^.*Mac OS X \([0-9.]*\) .*$/\1/"`
+osxVerMajMin="`echo $osxVerFul | cut -d. -f1-2`"
+fi
+
 mkdir -p $TMPDIR
 cd $TMPDIR
 svn checkout http://openjpeg.googlecode.com/svn/branches/openjpeg-1.5 openjpeg
 
-isOSX="`uname -s | grep -i Darwin`"
-osxVerFul=`system_profiler |grep 'System Version'| sed -e"s/^.*Mac OS X \([0-9.]*\) .*$/\1/"`
 cmake_options="\
  -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
  -DBUILD_JPWL:BOOL=ON \
  -DBUILD_MJ2:BOOL=ON \
- -DBUILD_JPIP:BOOL=OFF \
+ -DBUILD_JPIP:BOOL=ON \
  -DBUILD_THIRDPARTY:BOOL=ON \
  "
-# CMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.5
+# On apple let's build universal binaries
 if [ "$isOSX" != "" ]; then
-cmake_options="$cmake_options -DCMAKE_OSX_ARCHITECTURES:STRING=ppc;ppc64;i386;x86_64"
+if [ "$osxVerMajMin" = "10.4" ]; then
+cmake_options="$cmake_options -DCMAKE_OSX_ARCHITECTURES:STRING=ppc;i386 -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.4"
+elif [ "$osxVerMajMin" = "10.7" ]; then
+cmake_options="$cmake_options -DCMAKE_OSX_ARCHITECTURES:STRING=i386;x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.5"
+fi
 fi
 
 mkdir $TMPDIR/openjpeg-build
