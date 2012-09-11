325d3b3dbe7412109d6fff868a416878d634ddc5
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2012-09-11 07:20:58 +0000

    Reverse order

--- dev-utils/ctest_scripts/macminig4_openjpeg.sh
+++ dev-utils/ctest_scripts/macminig4_openjpeg.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# first update git clone on github:
+cd $HOME/Dashboards/git/trunk
+git svn rebase
+git push --mirror mathieu
+
+# now run the dashboards:
 cd $HOME/Dashboards/ctest_scripts && svn up
 
 for i in `ls macminig4-*.ctest`; do
   ctest -S $i -V -O $i.log 
 done
-
-# Update git clone on github:
-cd $HOME/Dashboards/git/trunk
-git svn rebase
-git push --mirror mathieu
