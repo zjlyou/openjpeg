74b79d98bb5464a4327ff4b973391a0b85d046d3
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-02-20 08:34:25 +0000

    Do not sync git from this machine anymore

--- dev-utils/ctest_scripts/macminig4_openjpeg.sh
+++ dev-utils/ctest_scripts/macminig4_openjpeg.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# first update git clone on github:
-cd $HOME/Dashboards/git/trunk
-git svn rebase
-git push --mirror mathieu
-
 # now run the dashboards:
 cd $HOME/Dashboards/ctest_scripts && svn up
 
