b662fdacc12fd83cc956fa32da77005142679fe1
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-02-26 13:54:26 +0000

    [dev-utils] Remove x64 dashboards since OS is 32bits

--- dev-utils/ctest_scripts/hpdesk_openjpeg.bat
+++ dev-utils/ctest_scripts/hpdesk_openjpeg.bat
@@ -11,16 +11,6 @@ for %%a in ("hpdesk-*nightly*x86.ctest") do call :concat "%%a"
 ctest -S %ctestlist% -V -O %ctestlist%.log
 endlocal
 
-setlocal
-@rem Use Visual Studio 2010 for x64
-call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
-
-@rem loop over all hpdesk-*x64.ctest files:
-set ctestlist=
-for %%a in ("hpdesk-*nightly*x64.ctest") do call :concat "%%a"
-ctest -S %ctestlist% -V -O %ctestlist%.log
-endlocal
-
 goto :EOF
 :concat
 set ctestlist=%ctestlist% %1
