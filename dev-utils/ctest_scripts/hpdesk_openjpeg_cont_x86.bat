f88ace2d1d5a03ff51feb8c267d6dcb93095c7e2
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-03-03 09:26:49 +0000

    [dev-utils] Run a continuous dashboard on trunk

--- dev-utils/ctest_scripts/hpdesk_openjpeg_cont_x86.bat
+++ dev-utils/ctest_scripts/hpdesk_openjpeg_cont_x86.bat
@@ -4,4 +4,4 @@ cd C:/Dashboards/ctest_scripts && svn up
 @rem Use Visual Studio 2010:
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-ctest -S hpdesk-b15-continuous-debug-win7_vs2010_x86.ctest -V -O cont_b15_x86.log
+ctest -S hpdesk-trunk-continuous-debug-win7_vs2010_x86.ctest -V -O cont_trunk_x86.log
