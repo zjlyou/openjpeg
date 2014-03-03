a88898711aa0187207ac8d22084f6d84531c2a9c
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-03-03 09:55:13 +0000

    [dev-utils] simplify for loop

--- dev-utils/ctest_scripts/hpdesk_openjpeg.bat
+++ dev-utils/ctest_scripts/hpdesk_openjpeg.bat
@@ -1,17 +1,9 @@
 @rem update ourself:
 cd C:/Dashboards/ctest_scripts && svn up
 
-setlocal
 @rem Use Visual Studio 2010:
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-@rem loop over all hpdesk-*x86.ctest files:
-set ctestlist=
-for %%a in ("hpdesk-*nightly*x86.ctest") do call :concat "%%a"
-ctest -S %ctestlist% -V -O %ctestlist%.log
-endlocal
-
-goto :EOF
-:concat
-set ctestlist=%ctestlist% %1
-goto :EOF
+@rem loop over all hpdesk-*x86.ctest files (nightly only)
+@echo off
+for %%f in (hpdesk-*nightly*x86.ctest) do ctest -S %%f -V -O %%f.log
