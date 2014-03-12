143321a603cb0331217097057c8f0540b0192aa0
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-03-12 12:09:30 +0000

    [dev-utils] Fix continuous dashboard after name change

--- dev-utils/ctest_scripts/hpdesk_openjpeg_cont_x86.bat
+++ dev-utils/ctest_scripts/hpdesk_openjpeg_cont_x86.bat
@@ -4,4 +4,4 @@ cd C:/Dashboards/ctest_scripts && svn up
 @rem Use Visual Studio 2010:
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-ctest -S hpdesk-trunk-continuous-debug-win7_vs2010_x86.ctest -V -O cont_trunk_x86.log
+ctest -S hpdesk-trunk-continuous-relwithdebinfo-win7_vs2010_x86.ctest -V -O cont_trunk_x86.log
