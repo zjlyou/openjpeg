121d8eb7bd3485a50cc7235ce9dc7e56542f7da1
mathieu.malaterre <mathieu.malaterre@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-03-13 16:36:21 +0000

    [dev-utils] Tweak uncrustify config file

--- dev-utils/scripts/clean.sh
+++ dev-utils/scripts/clean.sh
@@ -2,4 +2,5 @@
 
 #uncrustify  -c opj.cfg --no-backup --replace $1
 # remove tabs
-sed -e 's/\t/  /g' $1 | uncrustify -o $1.bla -l c -c opj.cfg
+#sed -e 's/\t/  /g' $1 | uncrustify -o $1.bla -l c -c opj.cfg
+sed -e 's/\t/  /g' $1 | uncrustify -o $1 -l c -c opj.cfg
