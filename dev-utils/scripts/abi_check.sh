fa5b533aa43d0e6f44fa3a0ce459cb089ba64f9e
mathieu.malaterre@gmail.com <mathieu.malaterre@gmail.com@974d547c-b4cf-11dd-89c7-03d08fec6209>
2014-04-24 16:48:07 +0000

    [dev-utils] Update to the latest trunk and run abi_check

--- dev-utils/scripts/abi_check.sh
+++ dev-utils/scripts/abi_check.sh
@@ -5,8 +5,8 @@ cd /tmp
 
 # Let's compare trunk ABI to tag 2.0
 svn co http://openjpeg.googlecode.com/svn/tags/version.2.0 opj20
-#svn co http://openjpeg.googlecode.com/svn/trunk opj21
-svn co https://openjpeg.googlecode.com/svn/branches/openjpeg-2.0 opj21
+svn co http://openjpeg.googlecode.com/svn/trunk opj21
+#svn co https://openjpeg.googlecode.com/svn/branches/openjpeg-2.0 opj21
 
 export CFLAGS='-fvisibility=hidden'
 for i in `ls -d /tmp/opj??`; do
@@ -18,5 +18,5 @@ done
 
 # check
 cd $cwd
-#abi-compliance-checker -l openjpeg -d1 abi_openjpeg_2.0.xml -d2 abi_openjpeg_2.1.xml
-abi-compliance-checker -l openjpeg -d1 abi_openjpeg_2.0.xml -d2 abi_openjpeg_2.0.1.xml
+abi-compliance-checker -l openjpeg -d1 abi_openjpeg_2.0.xml -d2 abi_openjpeg_2.1.xml
+#abi-compliance-checker -l openjpeg -d1 abi_openjpeg_2.0.xml -d2 abi_openjpeg_2.0.1.xml
